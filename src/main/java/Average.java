9a4039fecb979e1ed3e97cc6edd0866b29fd0249
rochoa13 <ochoarafael.jr@gmail.com>
2023-02-14 23:47:19 +0000

    Complete ArrayAverage

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,13 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+       int sum = 0;
+       double avg = 0;
+
+       for(int i = 0; i < nums.length; i++){
+        sum += nums[i];
+       } 
+       avg = (double)(sum/(double)nums.length);
+       return avg;
     }
 }
